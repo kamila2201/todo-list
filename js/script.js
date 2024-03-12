e9973c9384f785c6604f818173ad96adfe18f9b5
Kamila Maciąg <kamila.maciag22@gmail.com>
2024-03-12 16:32:32 +0100

    adding new task and showing if a task is dome

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@
 
         for (const task of tasks) {
             htmlString += `
-            <li>
+            <li class="${task.done ? "tasks__content--done" : ""}">
             ${task.content}
             </li>
             `;
@@ -24,9 +24,32 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+            if (newTaskContent === "") {
+                return;
+            }
+
+            addNewTask(newTaskContent);
+    };
+
 
     const init = () => {
         render();
+
+        const formElement = document.querySelector(".js-form");
+        formElement.addEventListener("submit", onFormSubmit);
     };
 
     init();
