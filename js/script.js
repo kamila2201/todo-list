0dfa822b6b3c12e995e1c57795404932157e39ff
Kamila Maciąg <kamila.maciag22@gmail.com>
2024-03-18 14:31:52 +0100

    formatted template strings

--- js/script.js
+++ js/script.js
@@ -47,11 +47,17 @@
 
         for (const task of tasks) {
             htmlString += `
-            <li class="tasks__item">
-            <button class="tasks__button tasks__button--toggleDone js-done">${task.done ? "✔" : ""}</button>
-            <span class="tasks__content ${task.done ? "tasks__content--done" : ""}">${task.content}</span>
-            <button class="tasks__button tasks__button--delete js-delete">🗑</button>
-            </li>
+              <li class="tasks__item">
+                <button class="tasks__button tasks__button--toggleDone js-done">
+                  ${task.done ? "✔" : ""}
+                </button>
+                <span class="tasks__content ${task.done ? "tasks__content--done" : ""}">
+                  ${task.content}
+                </span>
+                <button class="tasks__button tasks__button--delete js-delete">
+                  🗑
+                </button>
+              </li>
             `;
         }
 
